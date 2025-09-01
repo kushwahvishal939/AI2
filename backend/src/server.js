d379a1c3f28fbc7f0f9e4fcce02d89d9d0312c2f
Vishal Kushwah <vishal.kushwah@xemailverify.com>
2025-09-01 18:22:19 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,20 +39,6 @@ app.options('*', cors());
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-// Add CORS headers to all responses
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
 // API routes
 app.use('/api', routes)
 
